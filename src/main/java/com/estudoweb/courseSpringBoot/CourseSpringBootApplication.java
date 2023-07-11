149b6421123ea094d4039dfb12e4ec57900144bd
manoelerickgp <manoel.computacao@gmail.com>
2023-07-10 23:23:26 -0300

    Project created

---
+++ src/main/java/com/estudoweb/courseSpringBoot/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.estudoweb.courseSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootApplication.class, args);
+	}
+
+}
