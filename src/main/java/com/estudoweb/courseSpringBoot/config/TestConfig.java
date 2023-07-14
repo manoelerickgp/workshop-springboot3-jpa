5603b3703ec8b5e17e199684a0cd63ce7668d20c
manoelerickgp <manoel.computacao@gmail.com>
2023-07-13 23:28:21 -0300

    Adding OrderStatus enum

--- src/main/java/com/estudoweb/courseSpringBoot/config/TestConfig.java
+++ src/main/java/com/estudoweb/courseSpringBoot/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.estudoweb.courseSpringBoot.config;
 
 import com.estudoweb.courseSpringBoot.entities.Order;
 import com.estudoweb.courseSpringBoot.entities.User;
+import com.estudoweb.courseSpringBoot.entities.enums.OrderStatus;
 import com.estudoweb.courseSpringBoot.repositories.OrderRepository;
 import com.estudoweb.courseSpringBoot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,14 +34,14 @@ public class TestConfig implements CommandLineRunner {
         User user6 = new User(null, "Julie White", "julie@gmail.com", "482394839", "823948239");
         User user7 = new User(null, "Homer Simpson", "homer@gmail.com", "231948293", "234892304");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
-        Order order4 = new Order(null, Instant.parse("2020-02-11T10:50:33Z"), user5);
-        Order order5 = new Order(null, Instant.parse("2019-03-14T09:44:22Z"), user4);
-        Order order6 = new Order(null, Instant.parse("2019-05-18T18:25:11Z"), user6);
-        Order order7 = new Order(null, Instant.parse("2019-09-19T13:19:10Z"), user7);
-        Order order8 = new Order(null, Instant.parse("2019-03-09T15:58:59Z"), user3);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
+        Order order4 = new Order(null, Instant.parse("2020-02-11T10:50:33Z"), OrderStatus.DELIVERED, user5);
+        Order order5 = new Order(null, Instant.parse("2019-03-14T09:44:22Z"), OrderStatus.SHIPPED, user4);
+        Order order6 = new Order(null, Instant.parse("2019-05-18T18:25:11Z"), OrderStatus.CANCELED, user6);
+        Order order7 = new Order(null, Instant.parse("2019-09-19T13:19:10Z"), OrderStatus.PAID, user7);
+        Order order8 = new Order(null, Instant.parse("2019-03-09T15:58:59Z"), OrderStatus.WAITING_PAYMENT, user3);
 
         userRepository.saveAll(Arrays.asList(user1, user2, user3, user4, user5, user6, user7));
 
