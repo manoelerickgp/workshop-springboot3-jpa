d7703487a7af828657c3f8244e25e1940e53a929
manoelerickgp <manoel.computacao@gmail.com>
2023-07-14 23:59:32 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/estudoweb/courseSpringBoot/resources/CategoryResource.java
+++ src/main/java/com/estudoweb/courseSpringBoot/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import com.estudoweb.courseSpringBoot.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class CategoryResource {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> findById(Long id){
+    public ResponseEntity<Category> findById(@PathVariable Long id){
         Category obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
