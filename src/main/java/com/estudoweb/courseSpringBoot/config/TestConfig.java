d566fd6a6e8a6f88e967004dc599b016683146aa
manoelerickgp <manoel.computacao@gmail.com>
2023-07-19 19:58:05 -0300

    Exception handling - delete

--- src/main/java/com/estudoweb/courseSpringBoot/config/TestConfig.java
+++ src/main/java/com/estudoweb/courseSpringBoot/config/TestConfig.java
@@ -53,7 +53,7 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
         //
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-
+        //
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         User user3 = new User(null, "Bob Brown", "bob@gmail.com", "832984243", "12121212");
@@ -61,21 +61,17 @@ public class TestConfig implements CommandLineRunner {
         User user5 = new User(null, "Maria Silva", "maria@gmail.com", "123456789", "senha");
         User user6 = new User(null, "Julie White", "julie@gmail.com", "482394839", "823948239");
         User user7 = new User(null, "Homer Simpson", "homer@gmail.com", "231948293", "234892304");
-
+        //
         Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
         Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user2);
         Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
         Order order4 = new Order(null, Instant.parse("2020-02-11T10:50:33Z"), OrderStatus.DELIVERED, user5);
         Order order5 = new Order(null, Instant.parse("2019-03-14T09:44:22Z"), OrderStatus.SHIPPED, user4);
-        Order order6 = new Order(null, Instant.parse("2019-05-18T18:25:11Z"), OrderStatus.CANCELED, user6);
-        Order order7 = new Order(null, Instant.parse("2019-09-19T13:19:10Z"), OrderStatus.PAID, user7);
-        Order order8 = new Order(null, Instant.parse("2019-03-09T15:58:59Z"), OrderStatus.WAITING_PAYMENT, user3);
-
 
         //
         userRepository.saveAll(Arrays.asList(user1, user2, user3, user4, user5, user6, user7));
         //
-        orderRepository.saveAll(Arrays.asList(order1, order2, order3, order4, order5, order6, order7, order8));
+        orderRepository.saveAll(Arrays.asList(order1, order2, order3, order4, order5));
         //
         OrderItem oi1 = new OrderItem(order1, p1, 2, p1.getPrice());
         OrderItem oi2 = new OrderItem(order1, p3, 1, p3.getPrice());
@@ -83,11 +79,8 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(order3, p5, 2, p5.getPrice());
         OrderItem oi5 = new OrderItem(order4, p2, 1, p2.getPrice());
         OrderItem oi6 = new OrderItem(order5, p2, 1, p2.getPrice());
-        OrderItem oi7 = new OrderItem(order6, p3, 3, p3.getPrice());
-        OrderItem oi8 = new OrderItem(order7, p1, 2, p1.getPrice());
-        OrderItem oi9 = new OrderItem(order7, p4, 2, p4.getPrice());
         //
-        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4, oi5, oi6, oi7, oi8, oi9));
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4, oi5, oi6));
         //
         Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), order1);
         order1.setPayment(pay1);
